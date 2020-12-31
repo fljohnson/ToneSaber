c34e1bd16cf4b5bf79c0b8d3e4b7225d47d96287
Jess Mayers <jbmayers22@gmail.com>
2020-12-30 21:52:13 -0500

    start of redo of collision system
    (not full implementation) just first commit

---
+++ Assets/Scripts/Sphere.cs
@@ -0,0 +1,15 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Blade")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
