4af649df094c839b5f495d03a6758fcb8c3e9923
Jess Mayers <jbmayers22@gmail.com>
2020-12-30 22:06:37 -0500

    update of score implemenation (audio comming soon)

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Scores : MonoBehaviour
+public class ScoreManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static int score = 0;
+    private static int counter = 0;
+    private static void changeMusic()
     {
-        
+        if (counter <= 4)
+        {
+            //switch trigger
+        }
+        else if (counter > 4 && counter <= 8)
+        {
+            //switch trigger 2
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    public static void increaseScore(int amount)
     {
-        
+        score += amount;
+        counter++;
+        Debug.Log("Score: " + score);
+        //changeMusic();
+    }
+    public static int getScore()
+    {
+        return score;
     }
 }
--- Assets/Scripts/ToneTarget.cs
+++ Assets/Scripts/ToneTarget.cs
@@ -1,14 +1,14 @@
-﻿ using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sphere : MonoBehaviour
+public class ToneTarget : MonoBehaviour
 {
-
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.tag == "Blade")
         {
+            ScoreManager.increaseScore(1);
             Destroy(gameObject);
         }
     }
