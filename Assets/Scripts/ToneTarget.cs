df7d34229154de8527f99d4b9c57c95edf983291
Jess Mayers <jbmayers22@gmail.com>
2020-12-30 22:43:25 -0500

    working movement of blocks

--- Assets/Scripts/ToneTarget.cs
+++ Assets/Scripts/ToneTarget.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class ToneTarget : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+        Debug.Log(rb);
+        
+    }
+    void Update(){
+        rb.velocity = new Vector2(0.0f, -1.0f);
+    }
     void OnTriggerEnter2D(Collider2D col)
     {
+        //user collide with ToneTarget
         if (col.tag == "Blade")
         {
             ScoreManager.increaseScore(1);
